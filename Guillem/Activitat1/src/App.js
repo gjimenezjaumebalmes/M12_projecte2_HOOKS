6322acb821b6dcc80ee67ba4819de3f721d4ef2f
FHP187 <gjimenez@jaumebalmes.net>
2023-02-03 17:22:00 +0100

    2n Push activitat 1 finalitzada

--- Guillem/Activitat1/src/App.js
+++ Guillem/Activitat1/src/App.js
@@ -1,46 +1,11 @@
-import React from "react";
-import './App.css';
+import React, { useState } from "react";
 
-const Grupo = () => {
-  return (
-    <div>
-      <h1>Grupo E</h1>
-    </div>
-  )
-}
-
-const Person = () => {
-  return (
-    <div>
-      <h1>Guillem Jimenez</h1>
-      <h1>Luis Castillo</h1>
-      <h1>Raul Vaquerizo</h1>
-    </div>
+export default function App() {
+    const [showText, setShowText] = useState(false);
+    return (
+        <div align="center" className="App">
+            <button onClick={() => setShowText(!showText)}>Show/Hide</button>
+            {showText && <div>El text es mostra</div>}
+        </div>
     );
-};
-
-
-
-const TextInfo = () => {
-  return(
-    <div>
-      <input type="date" id="date" name="date"/>
-    </div>
-  )
 }
-
-function App() {
-  return (
-    <div className="App">
-		<label>FECHA INICIO </label>
-        <input type="date" id="date" name="date"/><p></p>
-        <label>FECHA FIN </label>
-        <input type="date" id="date" name="date"/><p></p>
-        <label>FECHA ENTREGA </label>
-        <input type="date" id="date" name="date"/>
-     
-    </div>
-  );
-}
-
-export default App;
