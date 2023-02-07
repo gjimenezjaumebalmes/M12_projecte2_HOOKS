f0cd100f3708fc89dbceb8f981c3a81825e3558a
FHP187 <gjimenez@jaumebalmes.net>
2023-02-07 18:14:46 +0100

    1s commit activitat 4

--- Guillem/Activitat4/src/App.js
+++ Guillem/Activitat4/src/App.js
@@ -1,16 +1,13 @@
 import {useRef, useState} from "react"
 import Button from "./Button";
+import './App.css';
 
 function App() {
-
     const [colorText, setColorText] = useState('green');
-
-    return(
-        <div align="center" className="App">
+    return (
+        <div className="App">
             <Button></Button>
-            <h1 style={{color: colorText}}>CANVI DE COLOR</h1>
         </div>
-      );
+    );
 }
-
-export default App
+export default App;
--- Guillem/Activitat4/src/Button.js
+++ Guillem/Activitat4/src/Button.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import { useState } from "react";
+import React, { Component }  from 'react';
 
 const Button = () => {
-    return(
-        <button onClick={() => {
-            setColorText(colorText == 'green' ? 'red' : 'green')
-        }}>Canvi
-        </button>
+    return (
+        <div className="App">
+            <button onClick={() => {
+                setColorText(colorText === 'green' ? 'red':'green')
+            }}>Canvi</button>
+            <h1 style={{color: colorText}}>CANVI DE COLOR</h1>
+        </div>
     );
 }
+
 export default Button;
